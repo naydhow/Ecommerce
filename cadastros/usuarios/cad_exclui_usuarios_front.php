aa13542742144ae22e75a98c156c78c4cb875f26
kaoriS2 <kaori.sakai@unesp.br>
2022-08-14 16:16:33 -0300

    Update cad_exclui_usuarios_front.php
    bta

--- cadastros/usuarios/cad_exclui_usuarios_front.php
+++ cadastros/usuarios/cad_exclui_usuarios_front.php
@@ -4,41 +4,53 @@
 </iframe>
 
 <?php
-       $cod_produto = $_GET["cod_produto"];
-       include "cad_getinfo_produto_back.php"; 
+       $id_usuario = $_GET["id_usuario"];
+       include "cad_getinfo_usuario_back.php"; 
 ?>
 
 <!-- Formulário (após as informações serem carregadas) -->
-<form action="cad_exclui_produtos_back.php" method="post">
-    <h1>Confirmação: Exclusão de Produtos</h1>
-    Id do produto
-    <input type="text" name="id_produto" 
-           value="<?php echo $linha['id_produto']; ?>" 
+<form action="cad_exclui_usuario_back.php" method="post">
+    <h1>Confirmação: Exclusão de Usuarios</h1>
+    Id do usuario
+    <input type="text" name="id_usuario" 
+           value="<?php echo $linha['id_usuario']; ?>" 
            readonly>
     <br><br>Nome
     <input type="text" name="nome" 
            value="<?php echo $linha['nome']; ?>" >
-    <br><br>Descrição
-    <input type="text" name="descricao" 
-           value="<?php echo $linha['descricao']; ?>" >
-    <br><br>Preço
-    <input type="text" name="preco" 
-           value="<?php echo $linha['preco']; ?>" >
-    <br><br>Custo
-    <input type="text" name="custo" 
-           value="<?php echo $linha['Custo']; ?>" >
-    <br><br>Margem de Lucro
-    <input type="text" name="margem_lucro" 
-           value="<?php echo $linha['margem_lucro']; ?>" >
-    <br><br>ICMS
-    <input type="text" name="icms" 
-           value="<?php echo $linha['icms']; ?>" >
-    <br><br>Estoque
-    <input type="text" name="estoque" 
-           value="<?php echo $linha['estoque']; ?>" >
+    <br><br>Telefone
+    <input type="text" name="telefone" 
+           value="<?php echo $linha['telefone']; ?>" >
+    <br><br>Email
+    <input type="text" name="email" 
+           value="<?php echo $linha['email']; ?>" >
+    <br><br>Senha
+    <input type="text" name="senha" 
+           value="<?php echo $linha['senha']; ?>" >
+    <br><br>Sexo
+    <input type="text" name="sexo" 
+           value="<?php echo $linha['sexo']; ?>" >
+    <br><br>Data de Nascimento
+    <input type="text" name="data_nasc" 
+           value="<?php echo $linha['data_nasc']; ?>" >
+    <br><br>CPF
+    <input type="text" name="cpf" 
+           value="<?php echo $linha['cpf']; ?>" >
+    <br><br>CEP
+    <input type="text" name="cep" 
+           value="<?php echo $linha['cep']; ?>" >
+    <br><br>Endereço
+    <input type="text" name="endereco" 
+           value="<?php echo $linha['endereco']; ?>" >
+    <br><br>Cidade
+    <input type="text" name="cidade" 
+           value="<?php echo $linha['cidade']; ?>" >
+    <br><br>Estado
+    <input type="text" name="estado" 
+           value="<?php echo $linha['estado']; ?>" >
 
      <br><br>
     <input type="submit" value="Confirma exclusão">
-    <input type="button" value="Editar" onclick="location.href='cad_altera_produtos_front.php?cod_produto=<?php echo $id_produto ?>';">
-    <input type="button" value="Voltar" onclick="location.href='cad_pesq_produtos_front.php';">
+    <input type="button" value="Editar" onclick="location.href='cad_altera_usuario_front.php?id_usuario=<?php echo $id_usuario ?>';">
+    <input type="button" value="Voltar" onclick="location.href='cad_pesq_usuario_front.php';">
 </form>
