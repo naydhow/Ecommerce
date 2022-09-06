550fc66e3890343bcfb7b23d0f8683d76cc51cbc
eduardaagarbullio <111603837+eduardaagarbullio@users.noreply.github.com>
2022-09-06 16:33:46 -0300

    div container_paginas

--- cadastros/produtos/cad_novo_produtos_front.php
+++ cadastros/produtos/cad_novo_produtos_front.php
@@ -33,8 +33,13 @@
     ?>
 
     <div class="container_paginas">
-        <p class="cadastro_titulos">Cadastro de Produtos</p>
-
+        <div class="titulo">
+            <!--<img class ="img_titulo" src="../../img/cacto.png">-->
+            <p class="cadastro_titulos">Cadastre-se na loja</p>
+             <!--<img class ="img_titulo" src="../../img/cacto.png">-->
+        </div>
+        
+ 
         <div class="form_cad_prod">             
             <form enctype="multipart/form-data" action="cad_novo_produtos_back.php" method="post">
                 <div class="input-group">
