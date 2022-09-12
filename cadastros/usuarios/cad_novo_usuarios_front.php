0d08525c2c3bd1a1363400054f66b77738a7c03a
eduardaagarbullio <111603837+eduardaagarbullio@users.noreply.github.com>
2022-09-12 10:47:53 -0300

    user cadastro

--- cadastros/usuarios/cad_novo_usuarios_front.php
+++ cadastros/usuarios/cad_novo_usuarios_front.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="utf-8" />
 
@@ -12,9 +13,10 @@
 
     <title>Formulário de Cadastro de Usuarios - Tabela Usuario CRUD</title>
 </head>
+
 <body>
     <?php
-        echo"
+    echo "
         <header id= 'header' >
             <nav class='container'>
                 <a  class='logo' href='../../index.php'><img src='../../img/logo.svg'></a>
@@ -31,137 +33,122 @@
                 <div class='toggle icon-menu'></div>
                 <div class='toggle icon-close'></div>
             </nav>
-        </header>"; 
+        </header>";
     ?>
 
     <div class="color_cadastros">
         <div class="container_paginas">
-            <p id="cadastro_titulos" >Cadastro de Usuários</p>
+            <p id="cadastro_titulos">Cadastro de Usuários</p>
 
             <div class="form_cad_user">
                 <form enctype="multipart/form-data" action="cad_novo_usuarios_back.php" method="post">
-                    
-               
-                        <div class="input-box-imagem">
-                            <div class="max-width-User">
-                                <div class="imageContainer"> 
+
+
+                    <div class="input-box-imagem">
+                        <div class="max-width-User">
+                            <div class="imageContainer">
                                 <img src="../../img/user.png" alt="Selecione uma foto para o perfil" id="imgPhoto">
-                                </div>
-                            </div>
-                            <div class="answer">
-                                <input name="ftperfil" id="ftperfil" type="file" accept="image/*">
                             </div>
                         </div>
+                        <div class="answer">
+                            <input name="ftperfil" id="ftperfil" type="file" accept="image/*">
+                        </div>
+                    </div>
 
-                    <div class="inputs_user">
+                    <div class="input-box">
+                        <label>
+                            <p class="campo_label">Nome:</p>
+                            <input class="input_user_grande" type="text" name="nome" maxlength="100" placeholder="Digite seu nome completo" />
+                        </label>
+                    </div>
+
+                    <div class="input-box">
+                        <label>
+                            <p class="campo_label">Email:</p>
+                            <input class="input_user_grande" type="email" name="email" maxlength="80" placeholder="xxxxxxxx@yyyyyy.com" />
+                        </label>
+                    </div>
+
+                    <div class="input-box">
+                        <label>
+                            <p class="campo_label">Senha:</p>
+                            <input class="input_user_grande" type="password" name="senha" maxlength="32" placeholder="***********" />
+                            </label>
+                    </div>
+                
+                <div class="input_block">
+                    <div class="input-box">
+                        <div class="campo_label_peq2">
+                            <b>Sexo:</b>
+                            <label><input type="radio" name="sexo" value="Feminino" checked />&nbsp;Feminino</label>
+                            <label><input type="radio" name="sexo" value="Masculino" />&nbsp;Masculino </label>
+                            <input type="radio" name="sexo" value="Prefiro não dizer" />&nbsp;Prefiro não dizer</label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="input_block">
+                    <div class="input-box">
+                        <label>
+                            <p class="campo_label_pequeno">CPF:</p>
+                            <input class="input_user_pequeno" type="text" name="cpf" id="cpf" placeholder="000.000.000-00" />
+                        </label>
+                    </div>
+
                         <div class="input-box">
-                                <label>
-                                    <p class="campo_label">Nome:</p>    
-                                    <input class="input_user_grande" type="text" name="nome" maxlength="100" placeholder="Digite seu nome completo"/>
-                                </label>
-                            </div>
-
-                            <div class="input-box">
-                                <label>
-                                    <p class="campo_label">Email:</p>
-                                    <input class="input_user_grande" type="email" name="email" maxlength="80" placeholder="xxxxxxxx@yyyyyy.com"/>
-                                </label>
-                            </div>
-
-                            <div class="input-box">
-                                <label>
-                                    <p class="campo_label">Senha:</p>
-                                    <input class="input_user_grande" type="password" name="senha" maxlength="32" placeholder="***********"/><br />
-                                    <br />
-                                </label>
-                            </div>
-
-                    </div>
-                            
-                        <div class="inputs_users">
-                            <div class="input-box">
-                                <label>
-                                    <p class="campo_label_peq2">Sexo:</p>
-                                    <div class="input_radio">
-                                        <input type="radio" name="sexo" value="Feminino" checked>&nbsp;Feminino</input>
-                                    </div>
-
-                                    <div class="input_radio">
-                                    <input type="radio" name="sexo" value="Masculino">&nbsp;Masculino</input>
-                                    </div>
-
-                                    <div class="input_radio">
-                                        <input class="input_radio" type="radio" name="sexo" value="Outro">&nbsp;Outro</input>
-                                    </div>
-
-                                    </label>
-                            </div>
+                            <label>
+                                <p class="campo_label_pequeno">Telefone:</p>
+                                <input class="input_user_pequeno" type="text" name="telefone" id="telefone" placeholder="(00) 00000-0000" />
+                            </label>
                         </div>
+                    </div>
 
-                        <div class="inputs_users">
-        
-                            <div class="input-box">
-                                    <label>
-                                        <p class="campo_label_pequeno">CPF:</p>
-                                        <input class="input_user_pequeno" type="text" name="cpf" id="cpf" placeholder="000.000.000-00"/>
-                                    </label>
-                                </div>
+                    <div class="input-box">
+                        <label>
+                            <p class="campo_label_pequeno">Data Nasc.:</p>
+                            <input class="input_user_pequeno" type="date" name="data_nasc" />
+                        </label>
+                    </div>
 
-                                <div class="input-box">
-                                    <label>
-                                        <p class="campo_label_pequeno">Telefone:</p>    
-                                        <input class="input_user_pequeno" type="text" name="telefone" id="telefone" placeholder="(00) 00000-0000"/>
-                                    </label>
-                                </div>
-                            </div>
 
-                            <div class="input-box">
-                                <label>
-                                    <p class="campo_label_pequeno">Data Nasc.:</p>
-                                    <input class="input_user_pequeno" type="date" name="data_nasc" />
-                                </label>
-                            </div>
+                    <div class="input-box">
+                        <label>
+                            <p class="campo_label_pequeno">CEP:</p>
+                            <input class="input_user_pequeno" type="text" name="cep" id="cep" placeholder="00000-000" />
+                        </label>
+                    </div>
 
-                            
-                            <div class="input-box">
-                                <label>
-                                    <p class="campo_label_pequeno">CEP:</p>
-                                    <input class="input_user_pequeno" type="text" name="cep" id="cep" placeholder="00000-000"/>
-                                </label>
-                            </div>
+                    <div class="input-box">
+                        <label>
+                            <p class="campo_label">Endereço:</p>
+                            <input class="input_user_grande" type="text" name="endereco" id="endereco" maxlength="150" />
+                        </label>
+                    </div>
 
-                            <div class="input-box">
-                                <label>
-                                    <p class="campo_label">Endereço:</p>
-                                    <input class="input_user_grande" type="text" name="endereco" id="endereco" maxlength="150"/>
-                                </label>
-                            </div>
+                    <div class="input-box">
+                        <label>
+                            <p class="campo_label_pequeno">Cidade:</p>
+                            <input class="input_user_medio" type="text" name="cidade" id="cidade" maxlength="30" />
+                        </label>
+                    </div>
 
-                            <div class="input-box">
-                                <label>
-                                    <p class="campo_label_pequeno">Cidade:</p>
-                                    <input class="input_user_medio" type="text" name="cidade" id="cidade" maxlength="30"/>
-                                </label>
-                            </div>
-
-                            <div class="input-box">
-                                <label>
-                                    <p class="campo_label_pequeno">Estado:</p>
-                                    <input class="input_user_peq2" type="text" name="estado" id="estado" maxlength="2"/>
-                                </label>
-                            </div>
-
-                            <div class="buttons">
-                                <input type="submit" name="button" class="button_crud" value="Enviar" />
-                                <a class="button_crud" href='cad_pesq_usuarios_front.php'>Voltar</a>
-                            </div>
+                    <div class="input-box">
+                        <label>
+                            <p class="campo_label_pequeno">Estado:</p>
+                            <input class="input_user_peq2" type="text" name="estado" id="estado" maxlength="2" />
+                        </label>
+                    </div>
 
                     </div>
-                        
-        
-                </form>
+                    
+                    <div class="buttons_user">
+                        <input type="submit" name="button" class="button_crud" value="Enviar" />
+                        <a class="button_crud" href='cad_pesq_usuarios_front.php'>Voltar</a>
+                    </div>
+             </form>
             </div>
         </div>
     </div>
 </body>
+
 </html>
