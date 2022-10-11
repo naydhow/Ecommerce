e364fe0d19cd81a2ec3de4e0e257c2c0317c3b7b
LoloCaricati <111867643+LoloCaricati@users.noreply.github.com>
2022-10-11 09:20:31 -0300

    sla

--- cadastros/produtos/cad_novo_produtos_front.php
+++ cadastros/produtos/cad_novo_produtos_front.php
@@ -14,106 +14,124 @@
          include_once "../../utils/header_2.php";
     ?>
 
-    <div class="color_cadastros">
-        <div class="container_paginas" id="topo">
-            
-            <div class="alinha_tituloimg">
-                <img class ="img_fofinhas" src="../../img/img_suculenta.svg">
-                <p id="titulo_fofo_usu">Cadastro de Produtos</p>
-                <img class ="img_fofinhas" src="../../img/img_cacto.svg">
-            </div>
-            
-    
-            <div class="form_cad_prod">             
-                <form enctype="multipart/form-data" action="cad_novo_produtos_back.php" method="post">
-                    <!-- <div class="campos"> -->
-
-                        <div class="input-box">
-                            <label>
-                                <div class="campo_label">Nome</div>  
-                                <input class="input_form_grande" type="text" name="nome"  placeholder="Nome" max required/>
-                            </label>
-                        </div>
-
-                        <div class="input-box">
-                            <label>
-                                <div class="campo_label">Descrição</div>
-                                <input class="input_form_grande" type="text" name="descricao"  placeholder="Descrição"/>
-                            </label>
-                        </div>
-
-
-                        <div class="input-box-imagem-prod">
-                                <div class="campo_label_pequeno">Imagem</div>
-                                    <div class="max-width-prod">
-                                        <div class="imageContainer">
-                                            <img src="../../img/camera.svg" alt="Selecione uma imagem para o produto" id="imgProduto">
-                                        </div>
-                                    </div>
-                                    <div class="answer">
-                                        <input name="ftproduto" id="ftproduto" type="file" accept="image/*">
-                                    </div>
-                        </div>
-
-
-                        <div class="input-box">
-                            <label>
-                                <p class="campo_label_pequeno">Cod. Visual:</p>
-                                <input  class="input_form_pequeno" type="text" name="codigovisual" maxlength="13" 
-                                placeholder="Código Visual" required/>
-                            </label>
-                        </div>
-                    
-
-                        <div class="input-box">
-                            <label>
-                                <div class="campo_label_pequeno">Estoque:</div>
-                                <input class="input_form_pequeno" type="number" name="estoque" placeholder="Estoque" required/>
-                            </label>
-                        </div>
-
-                       
-
-                     
-                        <div class="input-box">
-                            <label>
-                                <p class="campo_label_pequeno">Preço:</p>
-                                <input class="input_form_pequeno" type="text" name="preco"  placeholder="Preço" required/>
-                            </label>
-                        </div>
-
-
-                        <div class="input-box">
-                            <label>
-                                <p class="campo_label_pequeno">Custo:</p>
-                                <input class="input_form_pequeno" type="text" name="custo"  placeholder="Custo" required/>
-                            </label>
-                        </div>
-
-                        <div class="input-box">
-                            <label>
-                                <p class="campo_label_pequeno">M. Lucro:</p>
-                                <input class="input_form_pequeno" type="text" name="margem_lucro" placeholder="Margem lucro" required/>
-                            </label>
-                        </div>
-
-                        <div class="input-box">
-                            <label>
-                                <p class="campo_label_pequeno">ICMS:</p>
-                                <input class="input_form_pequeno" type="text" name="icms" placeholder="ICMS" required/>
-                            </label>
-                        </div>
-                   
-                    <div class="buttons">
-                        <input type="submit" name="button" class="button_crud" value="Enviar"/>
-                        <a class="button_crud" href='cad_pesq_produtos_front.php'>Voltar</a>
-                    </div>
-
-                </form>
-            </div>
+<div>
+        <div class="alinha_tituloimg">
+            <img class="img_fofinhas" src="../../img/img_suculenta.svg">
+            <p id="titulo_fofo_usu">&nbsp;&nbsp; Cadastro de Produtos</p>
+            <img class="img_fofinhas" src="../../img/img_cacto.svg">
         </div>
-    </div>
-  
+
+        <div class="borda_fora_form">
+            <ul>
+                <li>
+                    <div id="lado1_form">
+                        <form enctype="multipart/form-data" action="cad_novo_produtos_back.php" method="post">
+                            <br> <br>
+                        <div class="input-box-imagem-prod">
+                                <div class="max-width-prod">
+                                    <div class="imageContainer" id="margem_img_cam">
+                                        <img src="../../img/camera.svg" alt="Selecione uma imagem para o produto" id="imgProduto">
+                                    </div>
+                                </div>
+                                <div class="answer">
+                                    <input name="ftproduto" id="ftproduto" type="file" accept="image/*">
+                                </div>
+                            </div>
+
+                            <br>
+
+                            <div>
+                                <strong class="textos">NOME: </strong>
+                                <input type="text" name="nome" maxlength="100" placeholder="Digite seu nome completo" class="resposta">
+                            </div>
+                            <br> <br>
+                            <div>
+                                <strong class="textos">E-MAIL: </strong>
+                                <input type="email" name="email" maxlength="80" placeholder="xxxxxxxx@yyyyyy.com" class="resposta">
+                            </div>
+                            <br> <br>
+                            <div>
+                                <strong class="textos">SENHA: </strong>
+                                <input type="password" name="senha" maxlength="32" placeholder="***********" class="resposta">
+                            </div>
+                            <br> <br>
+                            <div>
+                                <strong class="textos">FONE: </strong>
+                                <input type="text" name="telefone" placeholder="(00) 00000-0000" class="resposta">
+                                <div>
+                                    <br> <br>
+
+                                    <div class="input_block">
+                                        <div class="input-box">
+                                            <div class="campo_label_peq2">
+                                                <strong class="textos">SEXO: </strong><br><br>
+                                                <label class="textos"><input type="radio" name="sexo" value="Feminino" checked />&nbsp;Feminino</label>&nbsp;
+                                                <label id="sexo"><input type="radio" name="sexo" value="Masculino" />&nbsp;Masculino </label>&nbsp;
+                                                <label id="sexo"><input type="radio" name="sexo" value="Prefiro não dizer" />&nbsp;Prefiro não dizer</label>&nbsp;
+                                            </div>
+                                        </div>
+                                    </div><br>
+
+                                    <div class="input-box">
+                                        <label>
+                                            <strong class="textos">DATA&nbsp;NASC.</strong>
+                                            <input id="datanasc" type="date" name="data_nasc" />
+                                        </label>
+                                    </div><br>
+
+                                    <div>
+                                        <strong class="textos">CPF: </strong>
+                                        <input type="text" name="cpf" maxlength="80" class="resposta" placeholder="000.000.000-00">
+                                    </div>
+                                    <br> <br>
+                                    <div>
+                                        <strong class="textos">CEP: </strong>
+                                        <input type="text" name="cep" placeholder="00000-000" class="resposta">
+                                    </div>
+                                    <br> <br>
+                                    <div>
+                                        <strong class="textos">ENDEREÇO: </strong>
+                                        <input type="text" name="endereco" maxlength="150" placeholder="Endereço" class="resposta">
+                                    </div>
+                                    <br> <br>
+                                    <div>
+                                        <strong class="textos">CIDADE: </strong>
+                                        <input type="text" name="cidade" maxlength="30" placeholder="Cidade" class="resposta">
+                                    </div>
+                                    <br> <br>
+                                    <div class="input-box">
+                                        <label>
+                                            <strong class="textos">ESTADO</strong>
+                                            <input id="estado" type="text" name="estado" id="estado" maxlength="2" placeholder="XX" />
+                                        </label>
+                                    </div>
+
+                                    <div>
+                                        <input type="submit" name="button" value="Enviar" class="btn_enviar">
+                                    </div>
+                                    <div class="voltarr">
+                                        <a href="../../login/login.html">Voltar</a>
+                                    </div>
+                        </form>
+
+                </li>
+                <li>
+                    <!--  LADO 2 -->
+                    <br>
+                    <div id="lado2_form">
+                        <div class="borda_rosa">
+                            <br><br>
+                            <img class="img_form" src="../../img/cacto_login.svg">
+                            <br> <br><br> <br>
+                            <img class="img_form" src="../../img/vaso_redondo_mini.svg">
+                        </div>
+                    </div>
+                </li>
+            </ul>
+
+        </div>
+    </div>    
+
     <?php
 
         include_once "../../utils/footer_2.php";
